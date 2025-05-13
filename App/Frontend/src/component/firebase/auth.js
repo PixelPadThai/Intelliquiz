aeba12378e0fc26ae997420bf2c28688f73a6506
Omkar Kathile <omkarkathile420@gmail.com>
2025-05-13 17:03:05 +0530

    Added Protected Routes

--- App/Frontend/src/component/firebase/auth.js
+++ App/Frontend/src/component/firebase/auth.js
@@ -1,6 +1,7 @@
 import {auth} from "./firebase";
 import { createUserWithEmailAndPassword,GoogleAuthProvider } from "firebase/auth";
 
+
 export const doCreateUserWithEmailAndPassword = async (email, password) =>{
   return createUserWithEmailAndPassword(auth, email, password); 
 };
